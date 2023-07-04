51e683e63af11dceb4188a0886e88fb4d76b4c9b
manar <manarergo3@gmail.com>
2023-07-04 10:18:23 +0100

    handling submission and mapping items

--- to_do_list_app/src/component/Todo.js
+++ to_do_list_app/src/component/Todo.js
@@ -2,12 +2,22 @@ import {useState} from "react";
 
 export default function Todo(){
     const [todo,setTodo]=useState()
-
+    const [todolist,setTodoList]=useState([])
     const handlechange=(event)=>{setTodo(event.target.value)}
+
+    const handlesubmit=(event)=>{
+        event.preventDefault()
+        let templist=todolist
+        templist.push(todo)
+        setTodoList(templist)
+        setTodo("")
+    }
     return <div>
-        <form>
+        <form onSubmit={handlesubmit}>
         <input value={todo} onChange={handlechange} type="text" />
         <button type="submit">Submit</button>
+
         </form>
+        {todolist.map((item)=>(<h3>{item}</h3>))}
     </div>
 }
